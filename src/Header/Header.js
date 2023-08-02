ac3fa6be960b00bde6f02b546e327094177d358e
Baila3 <bailaly16@gmail.com>
2023-08-01 21:12:27 -0400

    style logo

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -8,7 +8,19 @@ function Header() {
         <div classname='Title'>
           <h1 className='Title1'>PIZZA</h1>
           <h1 className='Title2'>GRECO</h1>
-          <h2 className='Title3'>OKA</h2>
+          <div className='dots'>
+            <span className='dot'></span>
+            <span className='dot'></span>
+            <span className='dot'></span>
+            <span className='dot'></span>
+            <span className='dot'></span>
+            <h2 className='Title3'>OKA</h2>
+            <span className='dot'></span>
+            <span className='dot'></span>
+            <span className='dot'></span>
+            <span className='dot'></span>
+            <span className='dot'></span>
+          </div>
         </div>
         {/* <img src={logo} className="App-logo" alt="logo" /> */}
         {/* <p>

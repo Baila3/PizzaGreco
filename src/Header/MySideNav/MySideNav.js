2fe381b6fb6d2cc26f3b923d89a9807751c93f88
Baila3 <bailaly16@gmail.com>
2023-08-03 23:40:53 -0400

    add menu items in sidebav

--- src/Header/MySideNav/MySideNav.js
+++ src/Header/MySideNav/MySideNav.js
@@ -1,20 +1,40 @@
 import SideNav, { Toggle, Nav, NavItem, NavIcon, NavText } from '@trendmicro/react-sidenav';
 import '@trendmicro/react-sidenav/dist/react-sidenav.css';
+import './MySideNav.css';
 
 function MySideNav() {
-    return ( <SideNav style={{color:"red"}}
+    return ( <SideNav id="sdb"
     onSelect={selected => {
         console.log(selected)
     }}
     >
-    <SideNav.Toggle/>
-    <SideNav.Nav defaultSelected="home" >
-        <NavItem>
+    <SideNav.Toggle />
+    <SideNav.Nav defaultSelected="home"  >
+        <NavItem eventKey="Entrées">
             <NavIcon>
-                <i className="fa fa-fw fa-home" style={{fontSize: "1.5em" }}/>
+            <i class="fa-solid fa-drumstick-bite" style={{fontsize: "1.5em" }}></i>
             </NavIcon>
-            <NavText>Home</NavText>
+            <NavText>Entrées</NavText>
         </NavItem>
+        <NavItem eventKey="Pizzas">
+            <NavIcon>
+            <i class="fa-solid fa-pizza-slice" style={{fonsize: "1.5em"}}></i>
+            </NavIcon>
+            <NavText>Pizzas</NavText>
+        </NavItem>
+        <NavItem eventKey="Casse-Croûte">
+            <NavIcon>
+                <i class="fa-solid fa-burger" style={{fontsize: "1.5em" }}></i>
+            </NavIcon>
+            <NavText>Casse-Croûte</NavText>
+        </NavItem>
+        <NavItem eventKey="Repas">
+            <NavIcon>
+            <i class="fa-solid fa-utensils" style={{fontsize: "1.5em" }}></i>
+            </NavIcon>
+            <NavText>Repas</NavText>
+        </NavItem>
+    
      </SideNav.Nav>
     </SideNav>
 )}
